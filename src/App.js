3e76a7a7bcf30350ac52cb90f4e5b5f7ab5b4a36
ruthvikvachhani <ruthvikvachhani@gmail>
2022-10-12 19:48:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Slider } from "new-range-slider";
+import { useState } from "react";
 
 function App() {
+  const [Red, setRed] = useState(0);
+  const [Green, setGreen] = useState(0);
+  const [Blue, setBlue] = useState(0);
+
+  const onChangeRed = (event) => {
+    setRed(event.target.value);
+  };
+  const onChangeGreen = (event) => {
+    setGreen(event.target.value);
+  };
+  const onChangeBlue = (event) => {
+    setBlue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="color"
+        style={{ backgroundColor: `rgb(${Red},${Green},${Blue})` }}
+      ></div>
+      <div>
+        <div>
+          <Slider
+            min={0}
+            max={255}
+            value={Red}
+            onChangeValue={onChangeRed}
+            label="Red"
+          />
+        </div>
+        <div>
+          <Slider
+            min={0}
+            max={255}
+            value={Green}
+            onChangeValue={onChangeGreen}
+            label="Green"
+          />
+        </div>
+        <div>
+          <Slider
+            min={0}
+            max={255}
+            value={Blue}
+            onChangeValue={onChangeBlue}
+            label="Blue"
+          />
+        </div>
+      </div>
     </div>
   );
 }
